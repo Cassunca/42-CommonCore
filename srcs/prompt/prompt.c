7440efa56226993bf795836fff54cb8c97f273cb
amyyrodri <yguuuto@gmail.com>
2025-12-21 13:16:17 -0300

    feat: add interpret vars type %u for user and %d for directory

--- srcs/prompt/prompt.c
+++ srcs/prompt/prompt.c
@@ -6,7 +6,7 @@
 /*   By: kamys <kamys@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/20 22:56:28 by kamys             #+#    #+#             */
-/*   Updated: 2025/12/21 10:16:44 by kamys            ###   ########.fr       */
+/*   Updated: 2025/12/21 13:13:53 by kamys            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,17 +38,84 @@ char	*interpret_escapes(char *s)
 	return (res);
 }
 
-char	*parse_ps1(char	*line)
+static size_t	prompt_len(t_env_table *env, char *s)
+{
+	size_t	len;
+	int		i;
+
+	len = 0;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == '%' && s[i + 1] == 'u')
+		{
+			len += ft_strlen(env_get(env, "USER"));
+			i += 2;
+		}
+		else if (s[i] == '%' && s[i + 1] == 'd')
+		{
+			len += ft_strlen(cwd_with_tilde(env));
+			i += 2;
+		}
+		else
+		{
+			len++;
+			i++;
+		}
+	}
+	return (len);
+}
+
+char	*interpret_vars(t_env_table *env, char *s)
+{
+	char	*res;
+	char	*val;
+	int		i;
+	int		j;
+
+	res = malloc(prompt_len(env, s) + 1);
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if (s[i] == '%' && s[i + 1])
+		{
+			if (s[i + 1] == 'u')
+				val = env_get(env, "USER");
+			else if (s[i + 1] == 'd')
+				val = cwd_with_tilde(env);
+			else
+			{
+				res[j++] = s[i++];
+				continue ;
+			}
+			while (*val)
+				res[j++] = *val++;
+			i += 2;
+		}
+		else
+			res[j++] = s[i++];
+	}
+	res[j] = '\0';
+	return (res);
+}
+
+char	*parse_ps1(t_env_table *env, char *line)
 {
 	char	*raw;
-	char	*ps1;
+	char	*escaped;
+	char	*final;
 
 	raw = ft_strtrim(line + 7, "\"\n");
 	if (!raw)
 		return (NULL);
-	ps1 = interpret_escapes(raw);
+	escaped = interpret_escapes(raw);
 	free(raw);
-	return (ps1);
+	if (!escaped)
+		return (NULL);
+	final = interpret_vars(env, escaped);
+	free(escaped);
+	return (final);
 }
 
 void	init_ps1(t_env_table *env)
@@ -74,7 +141,7 @@ void	init_ps1(t_env_table *env)
 		}
 		if (!ft_strncmp(line, "PROMPT=", 7))
 		{
-			ps1 = parse_ps1(line);
+			ps1 = parse_ps1(env, line);
 			env_set(env, "PS1", ps1);
 			free(line);
 			free(ps1);
