64c58ad3fb10732c88b81ba55cf365f7671e0354
amyyrodri <yguuuto@gmail.com>
2026-01-06 18:50:10 -0300

    fix: organization

--- include/prompt.h
+++ include/prompt.h
@@ -6,18 +6,12 @@
 /*   By: kamys <kamys@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/20 23:36:55 by kamys             #+#    #+#             */
-/*   Updated: 2026/01/22 11:26:43 by kamys            ###   ########.fr       */
+/*   Updated: 2026/01/22 11:29:02 by kamys            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PROMPT_H
 # define PROMPT_H
-
-# define RED     "\033[31m"
-# define GREEN   "\033[32m"
-# define YELLOW  "\033[33m"
-# define BLUE    "\033[34m"
-# define RESET   "\033[0m"
 
 # include "minishell.h"
 
